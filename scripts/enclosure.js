6ac3f94fae20e0b42cd4b681a966bbc882c9649f
felix.ravella <felix.ravella@gmail.com>
2023-06-11 19:21:34 +0200

    feat: handle create + edit

--- scripts/enclosure.js
+++ scripts/enclosure.js
@@ -44,15 +44,20 @@ function displayEnclosures(){
 }
 
 function toggleCreation(){
-  console.log('hidden')
   document.getElementById("enclosure-creation-container").classList.toggle("hidden")
   document.getElementById("create-enclosure-button").classList.toggle("hidden")
 }
 
 function startEdit(index) {
+  const item = enclosures[index]
+  console.log(item)
   document.getElementById("enclosure-creation-container").classList.toggle("hidden")
   document.getElementById("create-enclosure-button").classList.toggle("hidden")
-
+  document.getElementById("id").setAttribute('value', (item.id).toString())
+  document.getElementById("name").setAttribute('value', (item.name))
+  document.getElementById("shape").value = item.shape
+  document.getElementById("surface").setAttribute('value', (item.surface).toString())
+  document.getElementById("biome").value = item.biome
 }
 
 function requestDelete(index) {
@@ -61,14 +66,28 @@ function requestDelete(index) {
 
 function handleSubmit(event){
   event.preventDefault();
+  const isCreation = (event.target.id.value == -1);
+
+  // set id if it is a new item
+  const id = (isCreation) ? (enclosures[enclosures.length-1].id) +1 : parseInt(event.target.id.value);
   const payload = {
     name: event.target.name.value,
     shape: event.target.shape.value,
-    surface: event.target.surface.value,
+    surface: parseFloat(event.target.surface.value),
     biome: event.target.biome.value,
   }
-  console.log(payload)
-  enclosures.push({id: enclosures.length, ...payload})
+  if (isCreation) {
+    console.log('is creation')
+
+    enclosures.push({id, ...payload})
+  } else {
+    for (let i = 0; i < enclosures.length; i++){
+      if (enclosures[i].id === id){
+        const newItem = Object.assign(enclosures[i], payload);
+        enclosures[i] = newItem;
+      }
+    }
+  }
   displayEnclosures();
 }
 
